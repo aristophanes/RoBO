29b2955e757c35adc92406f47dc0b64b865134d1
Tulip PG <toolgalvao@hotmail.com>
2016-10-27 10:30:00 +0200

    adjust logre

--- examples/example_bo_freeze_logre.py
+++ examples/example_bo_freeze_logre.py
@@ -4,12 +4,9 @@ import cPickle as pickle
 from robo.acquisition.ei import EI
 from robo.maximizers.cmaes import CMAES
 import numpy as np
-#from robo.task.ml.logistic_regression_freeze import LogisticRegression
 from robo.task.ml.lasagne_logrg_task_freeze import LogisticRegression
 from robo.solver.freeze_thaw_bayesian_optimization import FreezeThawBO
-#from robo.solver.freeze_thaw_bayesian_optimization3 import FreezeThawBO
 from robo.models.freeze_thaw_model import FreezeThawGP
-#from robo.models.freeze_thaw_model_ec import FreezeThawGP
 from robo.maximizers.direct import Direct
 from robo.initial_design.init_random_uniform import init_random_uniform
 
@@ -18,25 +15,24 @@ def load_dataset():
     def load_mnist_images(filename):
         with gzip.open(filename, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=16)
-        # The inputs are vectors now, we reshape them 
+         
         data = data.reshape(-1, 1, 28, 28)
 
         return data / np.float32(256)
 
     def load_mnist_labels(filename):
-        # Read the labels in  binary format.
         with gzip.open(filename, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=8)
-        # The labels are vectors of integers now, that's exactly what we want.
+        
         return data
 
-    # We can now download and read the training and test set images and labels.
+    
     X_train = load_mnist_images('train-images-idx3-ubyte.gz')
     y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
     X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
     y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
 
-    # We reserve the last 10000 training examples for validation.
+    
     X_train, X_val = X_train[:-10000], X_train[-10000:]
     y_train, y_val = y_train[:-10000], y_train[-10000:]
 
